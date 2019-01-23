572aa98de95c001d93b160c136ad8b3d96ba5d89
asantentata <nta2k10@gmail.com>
2019-01-23 22:12:24 +0000

    added plots-didn't manage to combine separate  bits of code :(

--- ao_with_fft.py
+++ ao_with_fft.py
@@ -9,13 +9,14 @@ from matplotlib.colors import LogNorm
 
 
 #importing TIFF image into python and converting to array
-image = Image.open("./images/beads 10 20.tiff")
+image = Image.open("./Images/beads 10 20.tiff")
 imarray = np.asarray(image, dtype = 'uint16')
 
 
-mode = [str(' 3 '),str(' 4 '),str(' 5 '),str(' 6 '),str(' 7 '),str(' 8 '),str(' 9 '),str('10 '),str('11 '),str('12 '),str('13 ')]
-radians = [str(' 0'),str(' 4'),str(' 8'),str('12'),str('16'),str('20'),str('24'),str('28')]
+
 def av_intensity():
+	mode = [str(' 3 '),str(' 4 '),str(' 5 '),str(' 6 '),str(' 7 '),str(' 8 '),str(' 9 '),str('10 '),str('11 '),str('12 '),str('13 ')]
+ 	radians = [str(' 0'),str(' 4'),str(' 8'),str('12'),str('16'),str('20'),str('24'),str('28')]
 	mode_array = [[] for i in range(11)]
 	mode_count = 0	
 	for image in mode:		
@@ -23,12 +24,12 @@ def av_intensity():
 	# plotting the mean intensity of each image as we cycle through images	
 		count = 1		
 		for i in radians:
-			now_pic = Image.open("./images/beads "+ str(mode[mode_count]) +str(i)+".tiff")
+			now_pic = Image.open("./Images/beads "+ str(mode[mode_count]) +str(i)+".tiff")
 			mean_pixel = np.mean(now_pic)
 			intensity_vals.append(mean_pixel)
 			count = count + 1
-			print(mode[mode_count])
-			print(intensity_vals)
+			#print(mode[mode_count])
+			#print(intensity_vals)
 		mode_array[mode_count] = intensity_vals		
 		mode_count = mode_count + 1
 	return mode_array
@@ -54,6 +55,8 @@ def MakeMask(R, Cut):
 
 
 def av_freq():
+	mode = [str(' 3 '),str(' 4 '),str(' 5 '),str(' 6 '),str(' 7 '),str(' 8 '),str(' 9 '),str('10 '),str('11 '),str('12 '),str('13 ')]
+	radians = [str(' 0'),str(' 4'),str(' 8'),str('12'),str('16'),str('20'),str('24'),str('28')]
 	#Variables for mask
 	radius = 256
 	cut = 200  
@@ -71,7 +74,7 @@ def av_freq():
 	# plotting the mean intensity of each image as we cycle through images	
 		count = 1		
 		for i in radians:
-			image = Image.open("./images/beads "+ str(mode[mode_count]) +str(i)+".tiff")
+			image = Image.open("./Images/beads "+ str(mode[mode_count]) +str(i)+".tiff")
 			imarray = np.asarray(image, dtype = 'uint16')
 			fft2 = fftpack.fft2(imarray)
 			masked_image = fftpack.fftshift(fft2)*ring_mask 
@@ -79,17 +82,36 @@ def av_freq():
 			avfreq_vals.append(mean_pixel)
 			count = count + 1
 		mode_array[mode_count] = avfreq_vals
-		print(mode_count)
-		print(mode_array[mode_count])		
+		#print(mode_count)
+		#print(mode_array[mode_count])		
 		mode_count = mode_count + 1
 	#plot_spectrum(masked_image)
 		
-	return mode_array
 
+	return mode_array
+#Code to make subplots
+x = range(10)
+y = range(10)
+
+fig, ax = plt.subplots(nrows=2, ncols=6)
+
+
+for row in ax:
+	count_row = 1
+	for col in row:
+		count_col = 1
+		col.plot(range(1,9),(np.squeeze(av_freq()[count_row][:], axis=0)),'rx')
+		print(av_freq()[count_row][:])
+		count_row = count_row + 1
+	count_row = count_row + 1	
+plt.show()
+
+print(range(1,9))
+print((av_freq()[1][:]))
 #These are the functions that return the 11x8 arrays.		
 av_freq()
 av_intensity()
-print("shape of feq array is ",np.shape(av_freq()))
+#print("shape of feq array is ",np.shape(av_freq()))
 
 out_mask = MakeMask(radius,cut)
 in_mask = (MakeMask(radius, cut*0.6))
